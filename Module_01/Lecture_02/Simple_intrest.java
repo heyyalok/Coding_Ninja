c29a60178d0877a88277c3a1147ec9a7fb7209af
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-06-09 22:24:37 +0530

    Create Simple_intrest.java

---
+++ Module_01/Lecture_02/Simple_intrest.java
@@ -0,0 +1,12 @@
+package Lecture_02;
+public class Simple_intrest
+{
+	public static void main(String[]args)
+	{
+		int principle = 1000;
+		int time = 5;
+		int rate = 10;
+		int simple_intrest = (principle*time*rate)/100;
+		System.out.print(simple_intrest);
+	}
+}
