2829746c6564557afe9b2af351df2796bcd2c34a
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-06-12 15:45:18 +0530

    Create String_operations.java

---
+++ Module_01/Lecture_02/String_operations.java
@@ -0,0 +1,18 @@
+//import java.util.Scanner;
+package Lecture_02;
+import java.util.Scanner;
+
+public class String_operation 
+{
+	public static void main(String[]args)
+	{
+		Scanner scr = new Scanner(System.in);
+		System.out.println("input your full name");
+		String name = scr.next();
+		System.out.println("first name is "+name);
+		String last_name = scr.next();
+		System.out.println("second name is "+last_name);
+		//int  u = scr.nextInt();
+		//System.out.print("third name is "+u);
+	}
+}
