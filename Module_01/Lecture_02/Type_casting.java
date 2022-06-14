6ccb3f76dcdf1c8a0319c29c4b6e0e8b1ecb3f63
Alok kumar <65337995+heyyalok@users.noreply.github.com>
2022-06-14 18:51:49 +0530

    Create Type_casting.java

---
+++ Module_01/Lecture_02/Type_casting.java
@@ -0,0 +1,12 @@
+package Lecture_02;
+
+public class Type_casting {
+	public static void main (String[]args) 
+	{
+		//char ch = 'a';
+		//int ch2 = (char)(ch+1);
+		//System.out.println(ch2);
+		long i = 5646463497676L;
+		System.out.println(i);
+	}
+}
